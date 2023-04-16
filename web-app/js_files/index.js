51c32758f3d9ddbd5a1db575f4a62459d68d1f33
Josh Quinzon <quinzonjosh261@gmail.com>
2023-04-16 21:50:04 +0800

    styled pages

--- web-app/js_files/index.js
+++ web-app/js_files/index.js
@@ -5,29 +5,35 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // upon submit, create a movie object with the inputted data
     form.addEventListener('submit', event => {
-    event.preventDefault();
+        event.preventDefault();
 
-    const formData = new FormData(form);
-    
-    const movie_id = formData.get('movie-id')
-    const title = formData.get('title');
-    const year = formData.get('year');
-    const genre = formData.get('genre');
-    const director = formData.get('director');
-    const actor1 = formData.get('actor1');
-    const actor2 = formData.get('actor2');
+        const formData = new FormData(form);
+        
+        const movie_id = formData.get('movie-id')
+        const title = formData.get('title');
+        const year = formData.get('year');
+        const genre = formData.get('genre');
+        const director = formData.get('director');
+        const actor1 = formData.get('actor1');
+        const actor2 = formData.get('actor2');
 
-    const movieData = {
-        movie_id,
-        title,
-        year,
-        genre,
-        director,
-        actor1,
-        actor2
-    };
+        const movieData = {
+            movie_id,
+            title,
+            year,
+            genre,
+            director,
+            actor1,
+            actor2
+        };
 
-    console.log(movieData);
+        console.log(movieData);
+
+        fetch('localhost:9090/database/insert', {
+            method: 'POST',
+            body: formData
+          });
+
     });
 
     // Movie ID num only input validation
@@ -37,7 +43,6 @@ window.addEventListener('DOMContentLoaded', () => {
         const value = event.target.value;
         event.target.value = value.replace(/\D/g, "");
     });
-
     
 });
 
